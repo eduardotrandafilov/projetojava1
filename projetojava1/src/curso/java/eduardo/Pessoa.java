a21a7693bf2b8ef1b49b67678c8a66c6e15c1269
Eduardo Trandafilov <eduardo.trandafilov@gmail.com>
2025-03-25 13:17:10 -0300

    alteração e criação da classe pessoa

---
+++ projetojava1/src/curso/java/eduardo/Pessoa.java
@@ -0,0 +1,20 @@
+package curso.java.eduardo;
+
+public class Pessoa {
+
+	private String name;
+	private String sobrenome;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+}
