08226dada2481977d497335b7e1f4bea6ec2ccf0
Eduardo Trandafilov <eduardo.trandafilov@gmail.com>
2025-03-25 13:21:42 -0300

    adicionando o campo idade na classe pessoa

--- projetojava1/src/curso/java/eduardo/Pessoa.java
+++ projetojava1/src/curso/java/eduardo/Pessoa.java
@@ -4,7 +4,14 @@ public class Pessoa {
 
 	private String name;
 	private String sobrenome;
-	
+	private int idade;
+
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}	
 	public String getName() {
 		return name;
 	}
